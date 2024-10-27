7bc07016d20e7b71a50d84a521993752b9828b3e
Sergio Morales <sergiogmr@icloud.com>
2024-10-27 16:21:14 +0000

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,10 @@ export default defineConfig({
   site: process.env.CI
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
-  output:'server',
+    output: 'server',
+    build: {
+      outDir: './dist',
+    },
   integrations: [
     react(),
     vercel(),
